06d28f23927ff87d6cb8641ef8ac81dd258f8feb
Brian W. Davis <brianwdavis@gmail.com>
2024-03-21 17:00:39 -0400

    cleanup, function

--- pm3d_tables.R
+++ pm3d_tables.R
@@ -12,26 +12,31 @@ tabs <- table_endpoint(endp, sas = sas)
 # 
 # list_storage_tables(tabs)[["plantMap3dMetaDataPackage"]] %>% 
 #   list_table_entities()
+#   
+# storage_table(tabs, name = "plantMap3dMetaDataPackage") %>% 
+#   list_table_entities(as_data_frame = F)
 
+
+table_to_tibble <- function(tab_endp, nm) {
+  storage_table(tab_endp, name = nm) %>% 
+    list_table_entities(as_data_frame = F) %>% 
+    purrr::map(
+      ~purrr::map(.x, as.character) %>% 
+        as_tibble()
+      ) %>% 
+    bind_rows()
+}
 
 pm3d_alldata <- 
+  table_to_tibble(tabs, "plantMap3dMetaDataPackage") %>% 
   full_join(
-    list_storage_tables(tabs)[["plantMap3dMetaDataPackage"]] %>% 
-      list_table_entities() %>% 
-      as_tibble(),
-    list_storage_tables(tabs)[["plantMap3dMetaDataSet"]] %>% 
-      list_table_entities(as_data_frame = F) %>% 
-      purrr::map(~purrr::map(.x, as.character) %>% as_tibble()) %>% 
-      bind_rows(),
+    table_to_tibble(tabs, "plantMap3dMetaDataSet"),
     by = c("RowKey" = "PackageReferenceId"),
     keep = T,
     suffix = c("_PKG", "_SET")
   ) %>% 
   full_join(
-    list_storage_tables(tabs)[["plantMap3dMetaDataImage"]] %>% 
-      list_table_entities(as_data_frame = F) %>% 
-      purrr::map(~purrr::map(.x, as.character) %>% as_tibble()) %>% 
-      bind_rows(),
+    table_to_tibble(tabs, "plantMap3dMetaDataImage"),
     by = c("RowKey_SET" = "SetReferenceId"),
     keep = T,
     suffix = c("_SET", "_IMAGE")
@@ -39,7 +44,7 @@ pm3d_alldata <-
   mutate(
     ImageUrl = paste0('=HYPERLINK("', ImageUrl, '")'),
     ImageUrl = gs4_formula(ImageUrl)
-    )
+  )
 
 # View(pm3d_alldata)
 
