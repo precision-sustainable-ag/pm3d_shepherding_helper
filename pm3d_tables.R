8fcbfeb72c7b708af5b1a026ff29dca19fe4c702
Brian W. Davis <brianwdavis@gmail.com>
2024-03-21 16:19:15 -0400

    column rename, hyperlinks

--- pm3d_tables.R
+++ pm3d_tables.R
@@ -1,5 +1,7 @@
 library(AzureTableStor)
 library(dplyr)
+library(googlesheets4)
+
 
 source("secret.R")
 
@@ -14,25 +16,32 @@ tabs <- table_endpoint(endp, sas = sas)
 
 pm3d_alldata <- 
   full_join(
-  list_storage_tables(tabs)[["plantMap3dMetaDataPackage"]] %>% 
-    list_table_entities() %>% 
-    as_tibble(),
-  list_storage_tables(tabs)[["plantMap3dMetaDataSet"]] %>% 
-    list_table_entities(as_data_frame = F) %>% 
-    purrr::map(~purrr::map(.x, as.character) %>% as_tibble()) %>% 
-    bind_rows(),
-  by = c("RowKey" = "PackageReferenceId")
-) %>% 
+    list_storage_tables(tabs)[["plantMap3dMetaDataPackage"]] %>% 
+      list_table_entities() %>% 
+      as_tibble(),
+    list_storage_tables(tabs)[["plantMap3dMetaDataSet"]] %>% 
+      list_table_entities(as_data_frame = F) %>% 
+      purrr::map(~purrr::map(.x, as.character) %>% as_tibble()) %>% 
+      bind_rows(),
+    by = c("RowKey" = "PackageReferenceId"),
+    keep = T,
+    suffix = c("_PKG", "_SET")
+  ) %>% 
   full_join(
     list_storage_tables(tabs)[["plantMap3dMetaDataImage"]] %>% 
       list_table_entities(as_data_frame = F) %>% 
       purrr::map(~purrr::map(.x, as.character) %>% as_tibble()) %>% 
       bind_rows(),
-    by = c("RowKey" = "SetReferenceId")
-  )
+    by = c("RowKey_SET" = "SetReferenceId"),
+    keep = T,
+    suffix = c("_SET", "_IMAGE")
+  ) %>% 
+  mutate(
+    ImageUrl = paste0('=HYPERLINK("', ImageUrl, '")'),
+    ImageUrl = gs4_formula(ImageUrl)
+    )
 
-View(pm3d_alldata)
+# View(pm3d_alldata)
 
-library(googlesheets4)
 
 sheet_write(pm3d_alldata, ss = sheetid, sheet = "pm3d_alldata")
